3f0fb6edf45e70d527842e3a5f345bee5c3944e4
Feross Aboukhadijeh <feross@feross.org>
2013-12-14 19:31:15 -0800

    IE6-8 support

--- test/runner.js
+++ test/runner.js
@@ -15,15 +15,15 @@
 		],
 		res;
 
-	res = checks.some(function (check) {
+	res = some(checks, function (check) {
 		var b64Str,
 			arr,
 			str;
 
-		b64Str = b64.fromByteArray(Array.prototype.map.call(check, function (char) { return char.charCodeAt(0); }));
+		b64Str = b64.fromByteArray(map(check, function (char) { return char.charCodeAt(0); }));
 
 		arr = b64.toByteArray(b64Str);
-		str = arr.map(function (byte) { return String.fromCharCode(byte); }).join('');
+		str = map(arr, function (byte) { return String.fromCharCode(byte); }).join('');
 		if (check !== str) {
 			console.log('Fail:', check);
 			console.log('Base64:', b64Str);
@@ -37,3 +37,28 @@
 		console.log('All tests passed!');
 	}
 }());
+
+function some (arr, fun) {
+	for (var i = 0, len = arr.length; i < len; i++) {
+		if (i in arr && fun(arr[i], i, arr)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function map (arr, callback) {
+	var res = [];
+	for (var k = 0, len = arr.length; k < len; k++) {
+		if ((typeof arr === 'string' && !!arr.charAt(k))) {
+			var kValue = arr.charAt(k);
+			var mappedValue = callback(kValue, k, arr);
+			res[k] = mappedValue;
+		} else if (typeof arr !== 'string' && k in arr) {
+			var kValue = arr[k];
+			var mappedValue = callback(kValue, k, arr);
+			res[k] = mappedValue;
+		}
+	}
+	return res;
+}
