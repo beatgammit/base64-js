96c7a98bfb5c4ac10f30d60920e2ce8fc7c24b41
Dominic Tarr <dominic.tarr@gmail.com>
2013-12-30 11:48:57 +0700

    base64-js uses tabs, sorry!

--- test/bench.js
+++ test/bench.js
@@ -13,7 +13,7 @@ var end = Date.now()
 
 console.log('decode ms, decode ops/ms, encode ms, encode ops/ms')
 console.log(
-  middle - start,  data.length / (middle - start), 
-  end - middle,  data.length / (end - middle))
+	middle - start,  data.length / (middle - start), 
+	end - middle,  data.length / (end - middle))
 //console.log(data)
 
