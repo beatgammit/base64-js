fd04a98883079233c66a9d53c72b23944b63cf16
Feross Aboukhadijeh <feross@feross.org>
2018-02-15 18:41:19 -0800

    Add comment to document URL-safe base64 decoding
    Fixes https://github.com/beatgammit/base64-js/issues/40

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ for (var i = 0, len = code.length; i < len; ++i) {
   revLookup[code.charCodeAt(i)] = i
 }
 
+// Support decoding URL-safe base64 strings, as Node.js does.
+// See: https://en.wikipedia.org/wiki/Base64#URL_applications
 revLookup['-'.charCodeAt(0)] = 62
 revLookup['_'.charCodeAt(0)] = 63
 
