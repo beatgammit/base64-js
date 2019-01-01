7807f29c9cfa9141cb43cbb72d4264995fea6a6c
Jesse Tane <jesse.tane@gmail.com>
2018-12-31 21:34:25 -0500

    add test and api design for encoding to url-safe base64

--- test/url-safe.js
+++ test/url-safe.js
@@ -22,3 +22,15 @@ test('decode url-safe style base64 strings', function (t) {
 
   t.end()
 })
+
+test('encode url-safe style base64 strings', function (t) {
+  var expected = '__--_--_--__'
+
+  var bytes = [0xff, 0xff, 0xbe, 0xff, 0xef, 0xbf, 0xfb, 0xef, 0xff]
+  var actual = b64.fromByteArray(bytes, 'url')
+  for (var i = 0; i < actual.length; i++) {
+    t.equal(actual[i], expected[i])
+  }
+
+  t.end()
+})
