804d47e7eae4aaa5f764a847886c13ec9a078adb
Caleb Sander <caleb.sander@gmail.com>
2019-09-12 11:27:07 -0400

    Additional performance improvements

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ exports.byteLength = byteLength
 exports.toByteArray = toByteArray
 exports.fromByteArray = fromByteArray
 
-var lookup = []
-var revLookup = []
 var Arr = typeof Uint8Array !== 'undefined' ? Uint8Array : Array
 
-var code = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-for (var i = 0, len = code.length; i < len; ++i) {
-  lookup[i] = code[i]
-  revLookup[code.charCodeAt(i)] = i
+var lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+var revLookup = new Arr(128)
+for (var i = 0; i < lookup.length; ++i) {
+  revLookup[lookup.charCodeAt(i)] = i
 }
 
 // Support decoding URL-safe base64 strings, as Node.js does.
 // See: https://en.wikipedia.org/wiki/Base64#URL_applications
 revLookup['-'.charCodeAt(0)] = 62
 revLookup['_'.charCodeAt(0)] = 63
+
+// Number of bytes to encode as a base-64 chunk; must be multiple of 3
+var maxChunkLength = 16383
 
 function getLens (b64) {
   var len = b64.length
@@ -29,7 +30,7 @@ function getLens (b64) {
   // Trim off extra bytes after placeholder bytes are found
   // See: https://github.com/beatgammit/base64-js/issues/42
   var validLen = b64.indexOf('=')
-  if (validLen === -1) validLen = len
+  if (validLen < 0) validLen = len
 
   // 0 to 3 characters of padding so total length is a multiple of 4
   var placeHoldersLen = 3 - ((validLen + 3) & 3)
@@ -65,49 +66,48 @@ function toByteArray (b64) {
     : validLen
 
   var i
-  for (i = 0; i < len; i += 4) {
+  for (i = 0; i < len; i += 4, curByte += 3) {
     tmp =
-      revLookup[b64.charCodeAt(i)] << 18 |
+      revLookup[b64.charCodeAt(i    )] << 18 |
       revLookup[b64.charCodeAt(i + 1)] << 12 |
-      revLookup[b64.charCodeAt(i + 2)] << 6 |
+      revLookup[b64.charCodeAt(i + 2)] <<  6 |
       revLookup[b64.charCodeAt(i + 3)]
-    arr[curByte++] = tmp >> 16 & 0xFF
-    arr[curByte++] = tmp >> 8 & 0xFF
-    arr[curByte++] = tmp & 0xFF
+    arr[curByte    ] = tmp >> 16 & 0xFF
+    arr[curByte + 1] = tmp >>  8 & 0xFF
+    arr[curByte + 2] = tmp       & 0xFF
   }
 
   if (placeHoldersLen === 2) {
     arr[curByte] =
-      revLookup[b64.charCodeAt(i)] << 2 |
+      revLookup[b64.charCodeAt(i    )] << 2 |
       revLookup[b64.charCodeAt(i + 1)] >> 4
   } else if (placeHoldersLen === 1) {
     tmp =
-      revLookup[b64.charCodeAt(i)] << 10 |
-      revLookup[b64.charCodeAt(i + 1)] << 4 |
-      revLookup[b64.charCodeAt(i + 2)] >> 2
-    arr[curByte++] = tmp >> 8 & 0xFF
-    arr[curByte] = tmp & 0xFF
+      revLookup[b64.charCodeAt(i    )] << 10 |
+      revLookup[b64.charCodeAt(i + 1)] <<  4 |
+      revLookup[b64.charCodeAt(i + 2)] >>  2
+    arr[curByte    ] = tmp >> 8 & 0xFF
+    arr[curByte + 1] = tmp      & 0xFF
   }
 
   return arr
 }
 
 function tripletToBase64 (num) {
-  return lookup[num >> 18 & 0x3F] +
-    lookup[num >> 12 & 0x3F] +
-    lookup[num >> 6 & 0x3F] +
-    lookup[num & 0x3F]
+  return lookup[num >> 18       ] +
+         lookup[num >> 12 & 0x3F] +
+         lookup[num >>  6 & 0x3F] +
+         lookup[num       & 0x3F]
 }
 
 function encodeChunk (uint8, start, end) {
-  var tmp
   var output = new Array((end - start) / 3)
   for (var i = start, curTriplet = 0; i < end; i += 3) {
-    tmp =
-      (uint8[i] & 0xFF) << 16 |
-      (uint8[i + 1] & 0xFF) << 8 |
+    output[curTriplet++] = tripletToBase64(
+      (uint8[i    ] & 0xFF) << 16 |
+      (uint8[i + 1] & 0xFF) << 8  |
       (uint8[i + 2] & 0xFF)
-    output[curTriplet++] = tripletToBase64(tmp)
+    )
   }
   return output.join('')
 }
@@ -117,32 +117,27 @@ function fromByteArray (uint8) {
   var len = uint8.length
   var extraBytes = len % 3 // if we have 1 byte left, pad 2 bytes
   var len2 = len - extraBytes
-  var maxChunkLength = 16383 // must be multiple of 3
-  var parts = new Array(Math.ceil(len2 / maxChunkLength))
-
-  var curChunk = 0
+  var output = ''
 
   // go through the array every three bytes, we'll deal with trailing stuff later
   for (var i = 0, nextI; i < len2; i = nextI) {
-    nextI = i + maxChunkLength
-    parts[curChunk++] = encodeChunk(uint8, i, Math.min(nextI, len2))
+    nextI = Math.min(i + maxChunkLength, len2)
+    output += encodeChunk(uint8, i, nextI)
   }
 
   // pad the end with zeros, but make sure to not forget the extra bytes
   if (extraBytes === 1) {
-    tmp = uint8[len - 1] & 0xFF
-    parts[curChunk] =
-      lookup[tmp >> 2] +
-      lookup[tmp << 4 & 0x3F] +
-      '=='
+    tmp = uint8[len2] & 0xFF
+    output += lookup[tmp >> 2]
+    output += lookup[tmp << 4 & 0x3F]
+    output += '=='
   } else if (extraBytes === 2) {
-    tmp = (uint8[len - 2] & 0xFF) << 8 | (uint8[len - 1] & 0xFF)
-    parts[curChunk] =
-      lookup[tmp >> 10] +
-      lookup[tmp >> 4 & 0x3F] +
-      lookup[tmp << 2 & 0x3F] +
-      '='
+    tmp = (uint8[len2] & 0xFF) << 8 | (uint8[len2 + 1] & 0xFF)
+    output += lookup[tmp >> 10]
+    output += lookup[tmp >> 4 & 0x3F]
+    output += lookup[tmp << 2 & 0x3F]
+    output += '='
   }
 
-  return parts.join('')
+  return output
 }
