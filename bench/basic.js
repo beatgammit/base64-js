3ad6dc552e55143426876657a58ce8c74ad941bd
Feross Aboukhadijeh <feross@feross.org>
2020-10-28 17:39:31 -1000

    standard

--- bench/basic.js
+++ bench/basic.js
@@ -14,7 +14,10 @@ console.log(
   'decode ms, decode ops/ms, encode ms, encode ops/ms, length ms, length ops/ms'
 )
 console.log(
-  middle1 - start, data.length / (middle1 - start),
-  middle2 - middle1, data.length / (middle2 - middle1),
-  end - middle2, len / (end - middle2)
+  middle1 - start,
+  data.length / (middle1 - start),
+  middle2 - middle1,
+  data.length / (middle2 - middle1),
+  end - middle2,
+  len / (end - middle2)
 )
